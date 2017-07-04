c7e9f4a6d8be72634322105c74f674624c095344
Pavel <else@asus>
2017-07-04 12:58:32 +0300

    Интерфейс-блок размеров Проработан первоначальный рендеринг рамеров

---
+++ script/app.js
@@ -0,0 +1,39 @@
+"use script";
+
+var sizeList = [];
+
+rendering(sizeList);
+
+$('.btn-add').on('click', function () {
+	addToJson();
+	$('.inp-add').val('');
+
+});
+
+function addToJson () {
+	var str = $('.inp-add').val().trim(); //234 6
+	if ((str == 0) || (str == '') || (str == ' ')) return;
+
+	var pos = str.indexOf(' ');
+	if (pos == -1) {
+		sizeList.push({"size":str, "count":"1"});
+
+	} else {
+		var size = str.slice(0, pos);
+		var count = str.slice(pos + 1).trim();
+
+		sizeList.push({"size":size, "count":count});
+	}
+	
+	rendering(sizeList);
+}
+
+function rendering(sizeList) {
+	$('.details').empty();
+	$('.details').append('<tr><th>size</th><th>count</th></tr>');
+
+	sizeList.forEach(function (onceDetail) {
+		var contentWrap = '<tr><td>'+onceDetail.size+'</td>'+'<td>'+onceDetail.count+'</td></tr>';
+		$('.details'). append(contentWrap);
+	});
+}
